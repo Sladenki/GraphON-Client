b9908968faf1efd29e506d5c5404a454a0eddec5
MarkSakhar <mark.sahar18@mail.ru>
2025-02-03 15:08:47 +0200

    fix LittleTime Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,23 @@ import PostFeed from "@/components/ui/PostFeed/PostFeed";
 import { useAuth } from "@/providers/AuthProvider";
 import styles from './page.module.scss'
 
+
 const Homepage = () => {
-  const { isLoggedIn } = useAuth();
+
+  const { user } = useAuth();
+
+  const isAuth = user && Object.keys(user).length > 0;
+
 
   // Логика для выбора правильного URL запроса в зависимости от состояния isLoggedIn
-  const serverRequest = isLoggedIn ? 'post/getPostsAuth' : 'post/getPostsNoAuth';
+  const serverRequest = isAuth ? 'post/getPostsAuth' : 'post/getPostsNoAuth';
 
   return (
     <>
       {/* <p>Homepage 2 - ниже посты</p> */}
       <div className={styles.PostFeedWrapper}>
-        <PostFeed serverRequest={serverRequest} isLoggedIn={isLoggedIn} />
+        {/* @ts-expect-error */}
+        <PostFeed serverRequest={serverRequest} isLoggedIn={isAuth} />
       </div>
       
     </>
--- src/components/ui/PostFeed/PostFeed.tsx
+++ src/components/ui/PostFeed/PostFeed.tsx
@@ -5,6 +5,9 @@ import PostsList from "../PostsList/PostsList";
 import { useFetchBunchData } from "@/hooks/useFetchBunchData";
 import { SpinnerLoader } from "../SpinnerLoader/SpinnerLoader";
 import NoInfo from "../NoInfo/NoInfo";
+import { useAuth } from "@/providers/AuthProvider";
+import { useRouter } from "next/navigation";
+
 
 
 interface PostFeedProps {
@@ -13,9 +16,26 @@ interface PostFeedProps {
   isLoggedIn: boolean
 }
 
+// @ts-expect-error из-за return handleLogout()
 const PostFeed: FC<PostFeedProps> = ({serverRequest, isLoggedIn}) => {
 
+  const { logout } = useAuth();
+
+  const { push } = useRouter();
+
+  const handleLogout = async () => {
+    await logout();
+    push("/"); // Перенаправление на главную страницу после выхода
+  };
+
+
   const { allPosts, isPostsFetching, isEndPosts, loaderRef, error } = useFetchBunchData(serverRequest, [], isLoggedIn);
+
+  
+  if (error?.response?.status == 401) {
+   return handleLogout()
+  }
+
 
 
   return (
