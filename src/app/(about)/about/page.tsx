dbb4ec89667470815665e15456143a7c577352a6
MarkSakhar <mark.sahar18@mail.ru>
2025-08-02 22:34:03 +0200

    update file structure in aboutPage

--- src/app/(about)/about/page.tsx
+++ src/app/(about)/about/page.tsx
@@ -3,14 +3,14 @@
 import { motion } from 'framer-motion';
 import styles from './page.module.scss';
 import Link from 'next/link';
-import { HeroSection } from './components/HeroSection';
-import { FeaturesSection } from './components/FeaturesSection';
-import { HowItWorksSection } from './components/HowItWorksSection';
-import { CalendarDemo } from './components/CalendarDemo';
-import { TestimonialsSection } from './components/TestimonialsSection';
-import { CTASection } from './components/CTASection';
-import { Footer } from './components/Footer';
-import { AnimatedBackground } from './components/AnimatedBackground';
+import { HeroSection } from './components/HeroSection/HeroSection';
+import { FeaturesSection } from './components/FeaturesSection/FeaturesSection';
+import { HowItWorksSection } from './components/HowItWorksSection/HowItWorksSection';
+import { CalendarDemo } from './components/CalendarDemo/CalendarDemo';
+import { TestimonialsSection } from './components/TestimonialsSection/TestimonialsSection';
+import { CTASection } from './components/CTASection/CTASection';
+import { Footer } from './components/Footer/Footer';
+import { AnimatedBackground } from './components/AnimatedBackground/AnimatedBackground';
 
 const AboutPage = () => {
     return (
