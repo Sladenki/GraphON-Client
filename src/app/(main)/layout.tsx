9f276d41a7ab47b7449d505f681c69c487960aad
MarkSakhar <mark.sahar18@mail.ru>
2025-09-20 23:07:42 +0200

    remove googleAnalytics

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -4,12 +4,9 @@ import Sidebar from "@/components/global/Sidebar/Sidebar";
 import styles from './layout.module.scss'
 import BottomMenu from "@/components/global/BottomMenu/BottomMenu";
 import { AllProvers } from "@/providers/main";
-import type { Metadata } from 'next';
 import { inter, orbitron } from "@/app/fonts";
 import '../../styles/globals.scss'
 import { useMediaQuery } from "@/hooks/useMediaQuery";
-import Script from "next/script";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 import { Toaster } from "sonner";
 import { HeroUIProvider } from "@heroui/react";
 import ProfileCorner from "@/components/global/ProfileCorner/ProfileCorner";
@@ -26,27 +23,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
       <head>
         <title>GraphON</title>
 
-         {/* Подключаем Google Analytics только если есть ID */}
-         {/* {process.env.NEXT_PUBLIC_GA_ID && (
-          <>
-                          
-            <Script
-              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-              strategy="afterInteractive"
-            />
-            <Script id="google-analytics" strategy="afterInteractive">
-              {`
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-
-                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
-                  page_path: window.location.pathname,
-                });
-              `}
-            </Script>
-          </>
-        )} */}
       </head>
       <body className={inter.className}>
         <Providers>
@@ -65,8 +41,6 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
                 {/* Основная страница */}
                 <div className={styles.main}>
                   <div className={styles.content}>
-                    {/* Добавляем компонент для отслеживания переходов */}
-                    {/* <GoogleAnalytics />  */}
                     {children}
                   </div>
                 </div>
