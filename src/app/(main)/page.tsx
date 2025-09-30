7cce1e14a616e536613e7e5d5be0acd56975560a
MarkSakhar <mark.sahar18@mail.ru>
2025-09-30 22:06:16 +0200

    add HomePage

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,16 +1,7 @@
+import HomePage from './HomePage'
+
 export default function Home() {
-  return (
-    <div style={{
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-      minHeight: '60vh',
-      fontSize: '24px',
-      color: 'var(--main-text)'
-    }}>
-      main
-    </div>
-  );
+  return <HomePage />
 }
 
 
